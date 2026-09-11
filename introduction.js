f95023c8b2e07770fa18ccda74d9d4f204adeebd
Your Name <your-email@example.com>
2026-09-11 22:37:44 +0530

    Initial JavaScript learning files

---
+++ introduction.js
@@ -0,0 +1,9 @@
+// syntax of javascript //
+let name = " coding Artist jayesh";
+const age = 24;
+console.log(name);
+console.log(age);
+
+// external js file //
+console.log("this is external js file");
+console.log("Addition :"+(300+200));
