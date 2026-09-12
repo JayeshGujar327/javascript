057848ddd3f1cd5f676d0ce2ef6f635df2dc7a42
Jayesh Gujar <gujarj327@gmail.com>
2026-09-13 02:58:24 +0530

    Daily update

---
+++ functions.js
@@ -0,0 +1,18 @@
+
+// parameter and return type//
+function greet(name) {
+    return "hello: " + name;
+}
+
+let msg = greet("jay");
+
+console.log(msg);
+
+// return type with multiple parameters  // 
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(10, 20);
+
+console.log(result);
