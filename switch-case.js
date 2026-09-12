5a7415cbcd432007e0e42aee8b8617eb5b171dfd
Your Name <your-email@example.com>
2026-09-12 15:23:52 +0530

    Daily update

---
+++ switch-case.js
@@ -0,0 +1,32 @@
+// day name //
+let day = 3;
+switch(day)
+{
+    case 1:
+        console.log("monday ")
+        break;
+            case 2:
+        console.log("tuesday")
+        break;              
+case 3:
+        console.log("wednesday")
+        break;
+}
+
+
+// signal check//
+
+let signal =  "green";
+switch(signal){
+    case "red":
+        console.log("stop")
+        break;
+    case "yellow":
+        console.log("ready")
+        break;
+    case "green":
+        console.log("go")
+        break;  
+          default:
+        console.log("Invalid signal");      
+}
